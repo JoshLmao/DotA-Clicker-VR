11b9d31edaf186fa812e21140630553f90910e46
Josh Shepherd <josh_lmao@live.co.uk>
2016-08-01 20:44:56 +0100

    Added UI & Ambient sound looper

---
+++ DotA Clicker VR/Assets/Scripts/Controllers/AmbientSoundManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientSoundManager : MonoBehaviour {
+
+    public AudioClip[] AmbientClips;
+    public AudioSource AudioSource;
+
+	void Start ()
+    {
+        AudioSource = GameObject.Find("AmbientAudio").GetComponent<AudioSource>();
+        //InvokeRepeating("PlayAmbientSound", 1, 10f); //Comment out to stop audio playing
+    }
+
+	void Update ()
+    {
+        if (AudioSource.isPlaying) return;
+	}
+
+    void PlayAmbientSound()
+    {
+        int randomClips = Random.Range(0, AmbientClips.Length);
+        if (!AudioSource.isPlaying)
+        {
+            AudioSource.PlayOneShot(AmbientClips[randomClips]);
+            Debug.Log("Playing Ambient Sound");
+        }
+    }
+}
