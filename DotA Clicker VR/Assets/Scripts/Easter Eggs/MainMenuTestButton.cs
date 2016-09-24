f6a1cd6926295a44de105734e675157695265d13
Josh Shepherd <josh_lmao@live.co.uk>
2016-09-24 16:33:59 +0100

    Added props & Controls menu. Test Btn to main menu

---
+++ DotA Clicker VR/Assets/Scripts/Easter Eggs/MainMenuTestButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuTestButton : MonoBehaviour {
+
+    public AudioClip[] TestButtonResponses;
+
+    AudioSource m_audioSource;
+    Animator m_animator;
+
+	void Start ()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+        m_animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "ViveController")
+        {
+            PlayResponse();
+            m_animator.SetTrigger("isClicked");
+        }
+    }
+
+    void PlayResponse()
+    {
+        if (m_audioSource.isPlaying)
+            return;
+
+        int rng = Random.Range(0, TestButtonResponses.Length);
+        m_audioSource.PlayOneShot(TestButtonResponses[rng]);
+    }
+}
