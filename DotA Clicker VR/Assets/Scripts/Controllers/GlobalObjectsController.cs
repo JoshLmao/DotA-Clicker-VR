0e14b62149f3d9b82b672db0381ec143c210c378
Josh Shepherd <josh_lmao@live.co.uk>
2016-09-25 11:31:18 +0100

    Added loading screen and system to change between. Added Techies mines

---
+++ DotA Clicker VR/Assets/Scripts/Controllers/GlobalObjectsController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalObjectsController : MonoBehaviour
+{
+    public GameObject[] LoadingScreens;
+
+    GameObject m_loadingScreens;
+
+	void Start ()
+    {
+        m_loadingScreens = transform.Find("LoadingScreens").gameObject;
+
+        SetDontDestroys();
+        OnChangeScene(); //Set initial loading screen
+	}
+	
+	void Update ()
+    {
+	    
+	}
+
+    void SetDontDestroys()
+    {
+        DontDestroyOnLoad(gameObject);
+        foreach (Transform child in this.transform)
+        {
+            DontDestroyOnLoad(child.gameObject);
+        }
+    }
+
+    public void OnChangeScene()
+    {
+        int rng = Random.Range(0, LoadingScreens.Length);
+        
+        foreach(GameObject obj in LoadingScreens)
+        {
+            obj.SetActive(false);
+        }
+
+        LoadingScreens[rng].SetActive(true);
+    }
+}
